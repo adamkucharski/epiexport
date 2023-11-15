a4037762d49fd047688c94458aab994f4495c9a4
adamkucharski <adam.kucharski@lshtm.ac.uk>
2023-11-15 13:28:28 +0000

    Update comments in code

--- R/explore_estimation_wuhan.R
+++ R/explore_estimation_wuhan.R
@@ -1,3 +1,18 @@
+
+
+# OpenSkies data ----------------------------------------------------------
+
+# Load openSkies package (not used currently)
+library(openSkies)
+
+wuhan_departures <- getAirportDepartures(airport = "WUH", startTime = "2019-01-01 12:00:00", endTime = "2019-01-08 12:00:00",timeZone = "Europe/Berlin")
+
+
+# OpenFlights data --------------------------------------------------------
+
+flight_url <- "https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat"
+flight_data <- rio::import(file = flight_url) |> rename(source_airport=V3,destination_airport=V5)
+
 # OpenFlights guide
 # As of June 2014, the OpenFlights/Airline Route Mapper Route Database contains 67663 routes between 3321 airports on 548 airlines spanning the globe, as shown in the map above. Each entry contains the following information:
 # Airline 	2-letter (IATA) or 3-letter (ICAO) code of the airline.
@@ -10,30 +25,26 @@
 # Stops 	Number of stops on this flight ("0" for direct)
 # Equipment 	3-letter codes for plane type(s) generally used on this flight, separated by spaces
 
-library(openSkies)
+# Estimate Wuhan flights based on OpenFlights data
+wuhan_connections <- flight_data |> filter(source_airport=="WUH")
 
-# OpenFlights data
-flight_url <- "https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat"
-flight_data <- rio::import(file = flight_url) |> rename(source_airport=V3,destination_airport=V4)
+mean_flight_frequency_per_day <- 1/10 # User defined
+mean_passengers_per_flight <- 170 # User defined
 
-wuhan_flights <- flight_data |> filter(source_airport=="WUH")
 daily_outbound <- nrow(wuhan_flights)*mean_flight_frequency_per_day*mean_passengers_per_flight
 
-# OpenSkies data
-wuhan_departures <- getAirportDepartures(airport = "WUH", startTime = "2019-01-01 12:00:00", endTime = "2019-01-08 12:00:00",timeZone = "Europe/Berlin")
 
-# Imai et al (2020) values
+# Define Imai et al (2020) values for comparison
 daily_wuhan_iata <- 3301 # Value based on IATA data
 catchment_population <- 19e6
+
+# Natural history of epidemic
 incubation_period <- 5
 onset_to_detection <- 5
-  
 
-# Estimate Wuhan
 
-mean_flight_frequency_per_day <- 1/7 # User defined
-mean_passengers_per_flight <- 150 # User defined
-  
+# Case estimation model ---------------------------------------------------
+
 # Total cases domestically
 number_cases <- function(n_cases_abroad,
                          daily_outbound,
@@ -55,6 +66,11 @@ number_cases <- function(n_cases_abroad,
 # Imai et al estimate
 number_cases(3,daily_wuhan_iata,catchment_population,incubation_period,onset_to_detection)
 
-# OpenFlights estimate
+# OpenFlights estimate (aligned to roughly match Imai et al)
 number_cases(3,daily_outbound,catchment_population,incubation_period,onset_to_detection)
+
+
+# Other tools to explore: 
+# #https://opensky-network.org/data/data-tools#d4
+
 
